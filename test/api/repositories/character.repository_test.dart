f7679507250fc541ce0bf902b18b00d6b5ceb95c
Pontus Backman <ponbac@student.chalmers.se>
2021-08-13 19:19:08 +0200

    added initial files for character

---
+++ test/api/repositories/character.repository_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('testing character repository', () {
+    test('todo', () async {
+      expect(33, greaterThan(10));
+    });
+  });
+}
